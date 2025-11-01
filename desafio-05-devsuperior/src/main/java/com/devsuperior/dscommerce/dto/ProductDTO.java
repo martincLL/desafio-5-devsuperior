7283b9096d78995f22d2673b6fd151f881863d10
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2025-11-01 16:59:47 -0300

    Adicionando categories ProductDTO

--- desafio-05-devsuperior/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ desafio-05-devsuperior/src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dscommerce.dto;
 
+import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDTO {
 
@@ -19,8 +24,10 @@ public class ProductDTO {
     
     @Positive(message = "O preço deve ser positivo")
     private Double price;
-    
     private String imgUrl;
+
+    @NotEmpty(message = "Deve ter pelo menos uma categoria")
+    private List<CategoryDTO> categories = new ArrayList<>();
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -36,6 +43,9 @@ public class ProductDTO {
         description = entity.getDescription();
         price = entity.getPrice();
         imgUrl = entity.getImgUrl();
+        for(Category cat : entity.getCategories()) {
+            categories.add(new CategoryDTO(cat));
+        }
     }
 
     public Long getId() {
@@ -56,5 +66,9 @@ public class ProductDTO {
 
     public String getImgUrl() {
         return imgUrl;
+    }
+
+    public List<CategoryDTO> getCategories() {
+        return categories;
     }
 }
